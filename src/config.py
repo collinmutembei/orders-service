66c7870afe316ca936c991a75eaa119cfc5ed985
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2024-09-17 13:40:45 +0300

    Add messager adapter

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DEBUG: bool = config("DEBUG", default=False)
     DATABASE_URL: str = config("DATABASE_URL")
+    AFRICASTALKING_API_KEY: str = config("AFRICASTALKING_API_KEY")
+    AFRICASTALKING_USERNAME: str = config("AFRICASTALKING_USERNAME")
 
 
 settings = Settings()
